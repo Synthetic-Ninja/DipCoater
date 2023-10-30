e190fdc379d12d6af581998ba164479f1619dd3e
Тимофей Синягин <sinyagin_ta@rnd.stcnet.ru>
2023-10-30 22:13:53 +0300

    feat: add logger

---
+++ logger.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <Arduino.h>
+
+enum class LoggerLevel
+{
+    DEBUG,
+    INFO,
+    NO_LOG
+};
+
+
+class Logger
+{
+    public:
+        Logger(LoggerLevel log_level)
+        {
+            this->log_level = log_level;
+        }
+    
+    void debug(String message)
+    {
+        switch (log_level)
+        {
+            case LoggerLevel::NO_LOG:
+                break;
+        
+            case LoggerLevel::INFO:
+                break;
+
+            case LoggerLevel::DEBUG:
+                
+                Serial.println("__DEBUG__: " + message);
+                
+            default:
+                break;
+        }
+    }
+    void info(String message)
+    {
+         switch (log_level)
+        {
+            case LoggerLevel::NO_LOG:
+                break;
+        
+            case LoggerLevel::INFO:
+                Serial.println("__INFO__: " + message);
+
+            case LoggerLevel::DEBUG:
+                
+                Serial.println("__INFO__: " + message);
+                
+            default:
+                break;
+        }
+    }
+
+    void error(String message)
+    {
+         switch (log_level)
+        {
+            case LoggerLevel::NO_LOG:
+                break;
+        
+            case LoggerLevel::INFO:
+                Serial.println("__ERROR__: " + message);
+
+            case LoggerLevel::DEBUG:
+                
+                Serial.println("__ERROR__: " + message);
+                
+            default:
+                break;
+        }
+    }
+
+    private:
+       LoggerLevel log_level;
+
+};
